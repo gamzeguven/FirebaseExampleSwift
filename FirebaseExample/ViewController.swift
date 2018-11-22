9034c950ace98943ac5c9a6345112c09518aef1b
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2018-11-22 09:53:19 +0300

    update firebase to latest version

--- FirebaseExample/ViewController.swift
+++ FirebaseExample/ViewController.swift
@@ -10,23 +10,23 @@ class ViewController: UIViewController {
     
     @IBAction func buttonRegister(_ sender: UIButton) {
     
-        let email = textFieldEmail.text
-        let password = textFieldPassword.text
+        let emailRaw = textFieldEmail.text
+        let passwordRaw = textFieldPassword.text
         
-        FIRAuth.auth()?.createUser(withEmail: email!, password: password!, completion: { (user: FIRUser?, error) in
-
+        guard let email = emailRaw, let password = passwordRaw else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
             if error == nil {
                 self.labelMessage.text = "You are successfully registered."
             }else{
                 self.labelMessage.text = "Registration failed."
             }
-            
-        })
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //FIRApp.configure()
+        
     }
 
 }
